428ad08621d7ff0463d4452cf8615efed535f17b
nonrcs <noni@revenuescienceasia.com>
2012-11-28 14:44:21 +0700

    add devise gem

--- hotelwatcher/db/schema.rb
+++ hotelwatcher/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128050045) do
+ActiveRecord::Schema.define(:version => 20121128073740) do
 
   create_table "hotels", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,49 @@ ActiveRecord::Schema.define(:version => 20121128050045) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
+
+  create_table "price_watches", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "rate_from"
+    t.integer  "rate_to"
+    t.string   "location"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "room_types", :force => true do |t|
+    t.string   "bed_type"
+    t.text     "description"
+    t.integer  "normal_rate"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "season_prices", :force => true do |t|
+    t.integer  "roomtype_id"
+    t.integer  "price"
+    t.datetime "valid_from"
+    t.datetime "valid_to"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
