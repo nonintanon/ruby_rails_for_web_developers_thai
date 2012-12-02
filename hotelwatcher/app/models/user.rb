23cd8b51b77be940d93694986eb8b0cfd98a175c
nonrcs <noni@revenuescienceasia.com>
2012-12-02 21:47:18 +0700

    update authentication process

--- hotelwatcher/app/models/user.rb
+++ hotelwatcher/app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
+
+  def to_s
+  	name.present? ? name : email
+  end
+
 end
--- hotelwatcher/db/schema.rb
+++ hotelwatcher/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128092122) do
+ActiveRecord::Schema.define(:version => 20121202144358) do
 
   create_table "hotels", :force => true do |t|
     t.string   "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20121128092122) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
